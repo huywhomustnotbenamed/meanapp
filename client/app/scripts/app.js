7762fa3b7ebf681113f857cc7a13010be2b5e3c1
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-02 14:56:54 -0600

    movie add action

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -23,11 +23,6 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .when('/movies', {
         templateUrl: 'views/movies.html',
         controller: 'MoviesCtrl',
@@ -35,22 +30,18 @@ angular
       .when('/create/movie', {
         templateUrl: 'views/movie-add.html',
         controller: 'MovieAddCtrl',
-        controllerAs: 'movieAdd'
       })
       .when('/movie/:id', {
         templateUrl: 'views/movie-view.html',
         controller: 'MovieViewCtrl',
-        controllerAs: 'movieView'
       })
       .when('/movie/:id/delete', {
         templateUrl: 'views/movie-delete.html',
         controller: 'MovieDeleteCtrl',
-        controllerAs: 'movieDelete'
       })
       .when('/movie/:id/edit', {
         templateUrl: 'views/movie-edit.html',
         controller: 'MovieEditCtrl',
-        controllerAs: 'movieEdit'
       })
       .otherwise({
         redirectTo: '/'
