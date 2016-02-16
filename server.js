9391abde461f1bf71a7bc0f05770fd54e07e18b2
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-15 18:19:09 -0600

    added index.html

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app     = express();
 
-app.get('/', function(req, res){
-  res.send("Hello from server");
-});
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("running on port 3000");
