0485a3515bf67d34f9677330b611284646022e22
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-15 18:16:06 -0600

    set up server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app     = express();
+
+app.get('/', function(req, res){
+  res.send("Hello from server");
+});
+
+app.listen(3000);
+console.log("running on port 3000");
