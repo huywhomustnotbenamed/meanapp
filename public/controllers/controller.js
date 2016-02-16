dfc9436d210f3b9da82ce4af4bfef8fedb9a8234
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-15 21:50:04 -0600

    finished post requests

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,16 +2,22 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
 
+  var refresh = function(){
     $http.get('/contactlist').success(function(response){
       $scope.contactList = response;
+      $scope.contact     = "";
     });
+  };
 
-    $scope.addContact = function(){
-      console.log($scope.contact);
-      $http.post('/contactlist', $scope.contact)
-      .success(function(response){
-        console.log(response);
-      });
-    };
+  refresh();
+
+  $scope.addContact = function(){
+    console.log($scope.contact);
+    $http.post('/contactlist', $scope.contact)
+    .success(function(response){
+      console.log(response);
+      refresh();
+    });
+  };
 
 }]);
