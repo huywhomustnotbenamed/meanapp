87ed7860819d5a3f489f7ea88027db275e239b0d
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-16 16:50:32 -0600

    added server and webpack

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app     = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.all('/*', function(req, res){
+  res.send('Hello');
+});
+
+app.listen(PORT, function() {
+  console.log('Server running on ' + PORT);
+});
