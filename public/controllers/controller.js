5015229a9136cf58f877344a078d0fda09ec164b
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-15 18:53:15 -0600

    added data request

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,8 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+
+    $http.get('/contactlist').success(function(response){
+      $scope.contactList = response;
+    });
 }]);
--- server.js
+++ server.js
@@ -3,5 +3,22 @@ var app     = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function(req, res){
+  var personOne = {
+      name: 'One',
+      email: 'one@one.com',
+      number: '111'
+    };
+
+    var personTwo = {
+      name: 'two',
+      email: 'two@two.com',
+      number: '222'
+    };
+
+    var contactList = [personOne, personTwo];
+    res.json(contactList);
+});
+
 app.listen(3000);
 console.log("running on port 3000");
